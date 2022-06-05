2e68ad8cf88a3de48f676bfa85193c384d16a502
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-05 20:31:01 +0400

    'feat: add Course model'

---
+++ MicroServices/Catalog/FreeCourse.Services.Catalog/Entities/Course.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Entities
+{
+    public class Course : BaseEntity
+    {
+        [BsonRepresentation(BsonType.Decimal128)]
+        public double Price { get; set; }
+
+        public string Picture { get; set; }
+        public string Description { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        [BsonIgnore]
+        public Category Category { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
