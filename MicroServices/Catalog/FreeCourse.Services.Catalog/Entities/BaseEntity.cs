eed0ff8388ef157d487c7998e5f48fb34a794d51
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-05 20:26:39 +0400

    'feat: add BaseEntity model'

---
+++ MicroServices/Catalog/FreeCourse.Services.Catalog/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Entities
+{
+    public class BaseEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
