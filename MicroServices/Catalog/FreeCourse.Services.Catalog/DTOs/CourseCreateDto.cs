b5ce374da76c82f4937988755ab020f3a305b517
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-06 17:12:40 +0400

    'refactor: add Feature to CourseCreateDto inCatalog Microservice'

--- MicroServices/Catalog/FreeCourse.Services.Catalog/DTOs/CourseCreateDto.cs
+++ MicroServices/Catalog/FreeCourse.Services.Catalog/DTOs/CourseCreateDto.cs
@@ -1,4 +1,6 @@
-﻿namespace FreeCourse.Services.Catalog.DTOs
+﻿using FreeCourse.Services.Catalog.Entities;
+
+namespace FreeCourse.Services.Catalog.DTOs
 {
     public class CourseCreateDto
     {
@@ -6,7 +8,7 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string UserId { get; set; }
-
+        public Feature Feature { get; set; }
         public string CategoryId { get; set; }
     }
 }
