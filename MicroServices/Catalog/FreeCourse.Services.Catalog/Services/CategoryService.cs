bbcc8887dad0ce493523d171ff957692f5226618
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-07 21:43:51 +0400

    'feat: add CategoryService constructor into Catalog Microservice'

---
+++ MicroServices/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.DTOs;
+using FreeCourse.Services.Catalog.Entities;
+using FreeCourse.Services.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public class CategoryService
+    {
+        private readonly IMapper _mapper;
+        private readonly IMongoCollection<Category> _categoryCollection;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+
+    }
+}
